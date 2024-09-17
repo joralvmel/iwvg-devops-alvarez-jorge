8470f41a86a2817af89eb3b097d42a5570d414ae
joralvmel <joralvmel@gmail.com>
2024-09-17 08:39:06 +0200

    add Search 8 #13

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -28,4 +28,12 @@ public class Searches {
                 .map(User::getName)
                 .distinct();
     }
+
+    public Stream<String> findUserFamilyNameBySomeImproperFraction() {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(Fraction::isImproper))
+                .map(User::getFamilyName)
+                .distinct();
+    }
 }
